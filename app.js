5de9454a5007147d91a034ed80190f9506b32ed5
pedro <pedro092692@gmail.com>
2024-11-20 13:12:32 -0400

    inital commit

---
+++ app.js
@@ -0,0 +1,47 @@
+// define deck of cards
+deck = [ 
+    {
+        name: 'super-man',
+        img: 'img/super-man.jpg'
+    },
+    {
+        name: 'batman',
+        img: 'img/batman.jpg',
+    },
+    {
+        name: 'spider-man',
+        img: 'img/spider-man.jpg'
+    },
+    {
+        name: 'wonder-woman',
+        img: 'img/wonder-woman.jpg'
+    },
+    {
+        name: 'iron-man',
+        img: 'img/iron-man.jpg'
+    },
+    {
+        name: 'captain-america',
+        img: 'img/captain-america.jpg'
+    },
+    {
+        name: 'thor',
+        img: 'img/thor.jpg'
+    },
+    {
+        name: 'hulk',
+        img: 'img/hulk.jpg'
+    },
+    {
+        name: 'black-phanter',
+        img: 'img/black-phanter.jpg'
+    },
+    {
+        name: 'captain-marvel',
+        img: 'img/captain-marvel.jpg'
+    },
+    {
+        name: 'deadpool',
+        img: 'img/deadpool.jpg'
+    }
+]
