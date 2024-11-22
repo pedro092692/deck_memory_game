550f5084feb7013c89c13d9638ed344cd6255fb2
pedro <pedro092692@gmail.com>
2024-11-22 08:02:25 -0400

    add cards textures

--- js/app.js
+++ js/app.js
@@ -22,7 +22,9 @@ function draw_the_board(){
     const board = document.getElementById('deck');
     let cards = '';
     for(let card of deck){
-        cards += `<div class='${card.name}'> <img src='${card.img}' alt='${card.name}' height='110'> </div>`;
+        cards += `<div class='card'>
+                    <div class='${card.name}'> <img src='${card.img}' alt='${card.name}' height='110'> </div>
+                  </div>`;
     }
     board.innerHTML = cards;
 }
