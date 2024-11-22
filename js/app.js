f807e396a1d83fdb368cac73e87f35e6bd289090
pedro <pedro092692@gmail.com>
2024-11-22 07:37:38 -0400

    ajust board size based on game difficulty

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+//adjust board size based on difficulty
+function board_size(difficulty){
+    let board = document.getElementById('deck');
+    switch (difficulty){
+        case 4:
+            board.style.gridTemplateColumns='repeat(3, 1fr)';
+            board.style.gridTemplateRows='repeat(3, 1fr)';
+            break;
+        case 7:
+            board.style.gridTemplateColumns='repeat(4, 1fr)';
+            board.style.gridTemplateRows='repeat(4, 1fr)';
+            break;
+        case 11:
+            board.style.gridTemplateColumns='repeat(5, 1fr)';
+            board.style.gridTemplateRows='repeat(5, 1fr)';
+            break;    
+    }
+}
+
 // draw the deck
 function draw_the_board(){
     const board = document.getElementById('deck');
@@ -8,7 +27,12 @@ function draw_the_board(){
     board.innerHTML = cards;
 }
 
+
+
 //game events
+
+//adjust board size
+board_size(difficulty);
 
 //deal cards
 draw_the_board();
